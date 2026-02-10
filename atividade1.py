0adf891046844db46c50549fbd88e6c74484cb50
hiagoh2408-prog <hiago.h2408@gmail.com>
2026-02-10 14:57:41 -0300

    Create atividade1.py
    Primeira arquivo no github :)

---
+++ atividade1.py
@@ -0,0 +1,17 @@
+PRINT1 = "eu sei fazer print assim"
+PRINT2 = "e assim"
+print (PRINT1 + ": hello world ")
+print (PRINT2 + ": hello world")
+
+
+caracteres = "24"
+os_outros_caracteres = "7"
+print ("caracteres texto 1:" + caracteres)
+print ("caracteres texto 2:" +  os_outros_caracteres)
+
+
+restodoscaracteres = 24 % 7
+
+
+print  ("o resto dessas frases e:")
+print  (restodoscaracteres)
