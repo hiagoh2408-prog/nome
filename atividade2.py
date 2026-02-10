0e530988121824d86e585709032ff2579295ac14
hiagoh2408-prog <hiago.h2408@gmail.com>
2026-02-10 15:04:01 -0300

    Create atividade2.py
    Segundo arquivo:0

---
+++ atividade2.py
@@ -0,0 +1,9 @@
+numeroidade= 13
+textoidade= ("Minha idade ")
+#print (textoidade + str(numeroidade))
+altura = 1.55
+alturaeidade= altura + numeroidade
+#print("E minha altura " + str(altura) + " somado a minha idade resulta em " + str(alturaeidade))
+alturadepois= True
+idadedepois= True
+print ("minha idade depois do programa esta " + str(idadedepois) + " e minha altura depois do progama esta " + str(alturadepois))
