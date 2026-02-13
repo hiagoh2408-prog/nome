54bc3c34daf1b4c6ffd7c648b8354f0350892589
hiagoh2408-prog <hiago.h2408@gmail.com>
2026-02-13 15:52:14 -0300

    Create Atividade 4 com IF e ELSE.py
    muito dinheiro

---
+++ Atividade 4 com IF e ELSE.py
@@ -0,0 +1,11 @@
+custofrutas = 15
+custoderoupas = 50
+dinheirototal = 100
+if dinheirototal < custofrutas :  
+  print("Não vou levar as frutas pois não tenho dinheiro suficiente, preciso de mais " + str(custofrutas - dinheirototal) + " reais")
+else :
+  print("Vou levar as frutas, ainda vai sobrar " + str(dinheirototal - custofrutas) + " reais")
+if dinheirototal < custoderoupas :
+  print("Não posso levar as roupas, preciso de mais " + str(custoderoupas - dinheirototal) + " reais")
+else :
+  print("vou levar as roupas, pois ainda vai sobrar " + str(dinheirototal - custoderoupas) + " reais")custofrutas = 15
