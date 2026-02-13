d1d673c1fa6246ecd59e7d10b14b544c0028f90c
hiagoh2408-prog <hiago.h2408@gmail.com>
2026-02-13 15:34:25 -0300

    Create Atividade 3 IF e else.py
    Muitos IF's

---
+++ Atividade 3 IF e else.py
@@ -0,0 +1,9 @@
+chovendo = False
+nublado = True
+if chovendo == True :
+  print("Preciso do guarda chuva")
+else : 
+  if nublado == True :
+    print("Preciso do guarda chuva")
+  else :
+    print("Não vou levar um guarda chuva")
