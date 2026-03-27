a236abfc8842b05f1657e12876a2d087a22e16e9
hiagoh2408-prog <hiago.h2408@gmail.com>
2026-03-27 15:58:06 -0300

    Create atividade>=.py
    a

---
+++ atividade>=.py
@@ -0,0 +1,12 @@
+Nota = float(input("Digite sua nota: "))
+print(Nota)
+if Nota >= 9 :
+    print("A")
+elif Nota >= 8 :
+    print("B")
+elif Nota >= 7 :
+    print("C")
+elif Nota >= 6 :
+    print("D")
+else :
+    print("F")
