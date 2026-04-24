94f9df255108c1473a731c90d75c7bf6cac4c7d3
hiagoh2408-prog <hiago.h2408@gmail.com>
2026-04-24 15:27:06 -0300

    Update WhileATIV1.py
    a

--- WhileATIV1.py
+++ WhileATIV1.py
@@ -5,9 +5,9 @@ while Advinhando == False:
     if NumeroAdvinhado == Numero:
         print("Acertou")
         Advinhando == True
-    if NumeroAdvinhado > Numero:
+    elif NumeroAdvinhado > Numero:
         print("Número maior que o secreto")    
-    if NumeroAdvinhado < Numero:
+    elif NumeroAdvinhado < Numero:
         print("Número menor que o secreto")
     NumeroAdvinhado = int(input("Advinhe um número entre 1 a 10:  "))
 
