175e7636a64fa329ef2a9cd5bc85c89759bfb3e8
hiagoh2408-prog <hiago.h2408@gmail.com>
2026-05-29 15:13:26 -0300

    Create AtvFOR2.py
    aqa

---
+++ AtvFOR2.py
@@ -0,0 +1,10 @@
+Jogos = ["Minecraft","Skyrim","FIFA","Forsaken"]
+for Jogo in Jogos:   
+    if Jogo == Jogos[0]:
+        print(Jogos[0] + ", é meu favorito")
+    elif Jogo == Jogos[1]:
+        print(Jogos[1] + ", acho bom")
+    elif Jogo == Jogos[2]:
+        print(Jogos[2] + ", jogo de vez em quando")
+    else :
+        print(Jogos[3] + ", detesto")
